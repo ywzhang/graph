a45d7c92298cd1b5ee54ddabec5754c6ea2749ec
zhangyw <yongweizhang@163.com>
2019-11-06 15:41:33 +0800

    graph

--- src/main/java/com/xbstar/graph/controller/GraphController.java
+++ src/main/java/com/xbstar/graph/controller/GraphController.java
@@ -14,6 +14,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -33,21 +34,9 @@ public class GraphController {
     public static String nodesPath = "Json\\Nodes.json";
     public static String linksPath = "Json\\Links.json";
     public static String owlPath = "pension.ttl";
-
-    public static JSONArray NODE = new JSONArray();
-    public static JSONArray LINK = new JSONArray();
-
     public static OntModel m = ModelFactory.createOntologyModel();
 
-//    public static void main(String[] args) throws IOException {
-//        //SpringApplication.run(GraphApplication.class, args);
-//        m.read(owlPath);
-//        //listClass();
-//        //getInstanceByClass("City");
-//        getInstanceDetailByID("无锡市@3202");
-//        //getInstanceDetailByID("张三");
-//        System.out.println("Finish......");
-//    }
+    public static JSONObject listClass = new JSONObject();
 
 
     @RequestMapping("/index")
@@ -65,6 +54,8 @@ public class GraphController {
     @RequestMapping("/listClass")
     @ResponseBody
     public static JSONObject listClass() throws IOException {
+        JSONArray NODE = new JSONArray();
+        JSONArray LINK = new JSONArray();
         m.read(owlPath);
         JSONObject dataJSon = new JSONObject(true);
         JSONArray categories = new JSONArray();
@@ -129,24 +120,55 @@ public class GraphController {
         writeToJson(nodesPath, NODE);
         writeToJson(linksPath, LINK);
 
+        listClass = dataJSon;
+
         return dataJSon;
     }
 
     @RequestMapping("/getInstanceByClass")
     @ResponseBody
     public static JSONObject getInstanceByClass(String className) throws IOException {
+        JSONArray NODE = new JSONArray();
+        JSONArray LINK = new JSONArray();
+        OntClass ontClass = m.getOntClass(NS + className);
+
+        listClass.getJSONArray("nodes").stream().forEach(item->{
+            //System.out.println(item.getClass());
+            JSONObject json = JSONObject.parseObject(item.toString());
+            //JSONObject js = (JSONObject) item;
+            //System.out.println(ontClass.getLocalName());
+            if(json.getString("id").equals(ontClass.getLocalName())) {
+                JSONObject concept = new JSONObject();
+                concept.put("id", ontClass.getLocalName());
+                concept.put("category", 0);
+                concept.put("name", ontClass.getLocalName());
+                concept.put("label", ontClass.getLocalName());
+                concept.put("symbolSize", 37);
+                System.out.printf(ontClass.getLocalName());
+                NODE.add(concept);
+            } else {
+                JSONObject j = new JSONObject();
+                j.put("opacity", 0.3);
+                json.put("itemStyle",j);
+                NODE.add(json);
+            }
+        });
+
+        listClass.getJSONArray("links").stream().forEach(item->{
+            LINK.add(item);
+        });
         m.read(owlPath);
-        listClass();
+//        listClass();
+
         JSONObject dataJSon = new JSONObject(true);
         JSONArray nodes = new JSONArray();
         JSONArray links = new JSONArray();
         JSONArray categories = new JSONArray();
 
-        OntClass ontClass = m.getOntClass(NS + className);
+        //OntClass ontClass = m.getOntClass(NS + className);
         //System.out.printf(ontClass.getLocalName().toString());
         for(Iterator instances = ontClass.listInstances(); instances.hasNext(); ) {
             Individual individual = (Individual) instances.next();
-            //System.out.println(individual.toString().split("#")[1]);
 
             JSONObject node = new JSONObject(true);
             JSONObject link = new JSONObject(true);
@@ -155,7 +177,7 @@ public class GraphController {
             node.put("category", 1);
             node.put("name", individual.toString().split("#")[1]);
             node.put("label", individual.toString().split("#")[1].split("@")[0]);
-            node.put("symbolSize", 20);
+            node.put("symbolSize", 31);
 
             link.put("source", ontClass.getLocalName().toString());
             link.put("target", individual.toString().split("#")[1]);
@@ -190,9 +212,34 @@ public class GraphController {
     @RequestMapping("/getInstanceDetailByID")
     @ResponseBody
     public static JSONObject getInstanceDetailByID (String id) throws IOException {
+        JSONArray NODE = new JSONArray();
+        JSONArray LINK = new JSONArray();
         m.read(owlPath);
-        listClass();
+//        listClass();
         Individual individual = m.getIndividual(NS + id);
+
+        listClass.getJSONArray("nodes").stream().forEach(item->{
+            JSONObject json = (JSONObject) item;
+            if(json.getString("id") == individual.getOntClass().toString().split("#")[1]) {
+                JSONObject concept = new JSONObject();
+                concept.put("id", individual.toString().split("#")[1]);
+                concept.put("category", 1);
+                concept.put("name", individual.toString().split("#")[1]);
+                concept.put("label", individual.toString().split("#")[1].split("@")[0]);
+                concept.put("symbolSize", 37);
+                NODE.add(concept);
+            } else {
+                JSONObject j = new JSONObject();
+                j.put("opacity", 0.3);
+                json.put("itemStyle",j);
+                NODE.add(json);
+            }
+        });
+        listClass.getJSONArray("links").stream().forEach(item->{
+            LINK.add(item);
+        });
+
+
         JSONObject dataJSon = new JSONObject(true);
         JSONArray categories = new JSONArray();
         JSONArray nodes = new JSONArray();
@@ -207,7 +254,7 @@ public class GraphController {
         instanceNode.put("category", 1);
         instanceNode.put("name", individual.toString().split("#")[1]);
         instanceNode.put("label", individual.toString().split("#")[1].split("@")[0]);
-        instanceNode.put("symbolSize", 20);
+        instanceNode.put("symbolSize", 38);
 
         instanceLink.put("source", individual.getOntClass().toString().split("#")[1]);
         instanceLink.put("target", instanceNode.getString("id"));
@@ -215,21 +262,31 @@ public class GraphController {
 
         nodes.add(instanceNode);
         links.add(instanceLink);
-
+        String filterDP = null;
+        String filterOP = null;
+        if(instanceLink.getString("source").equals("City") || instanceLink.getString("source").equals("Province") || instanceLink.getString("source").equals("Country") || instanceLink.getString("source").equals("District")){
+            System.out.printf(instanceLink.getString("source"));
+            filterOP = "AddressOP";
+            filterDP = "AddressDP";
+        } else {
+            filterOP = "HumanOP";
+            filterDP = "HumanDP";
+        }
         while (stmtIterator.hasNext()){
             JSONObject node = new JSONObject(true);
             JSONObject link = new JSONObject(true);
             Statement statement = stmtIterator.nextStatement();
 
             // why get the AddressOP, HumanDP
-            if(!statement.getPredicate().getLocalName().equals("AddressOP") && !statement.getPredicate().getLocalName().equals("AddressDP")) {
+            if(!statement.getPredicate().getLocalName().equals(filterOP.toString()) && !statement.getPredicate().getLocalName().equals(filterDP.toString()
+            )) {
                 //System.out.println(statement);
                 if(statement.getObject().isLiteral()) {
                     node.put("id", UUID.randomUUID().toString().replace("-", ""));
                     node.put("category", 2);
                     node.put("name", statement.getObject().asLiteral().getValue().toString());
                     node.put("label", statement.getObject().asLiteral().getValue().toString());
-                    node.put("symbolSize", 15);
+                    node.put("symbolSize", 30);
 
                     link.put("source", individual.toString().split("#")[1]);
                     link.put("target", node.getString("id"));
@@ -245,7 +302,7 @@ public class GraphController {
                     node.put("category", 1);
                     node.put("name", statement.getObject().asResource().toString().split("#")[1]);
                     node.put("label", statement.getObject().asResource().toString().split("#")[1]);
-                    node.put("symbolSize", 20);
+                    node.put("symbolSize", 33);
 
                     link.put("source", individual.toString().split("#")[1]);
                     link.put("target", node.getString("id"));
@@ -289,8 +346,8 @@ public class GraphController {
         });
 
         dataJSon.put("categories",categories);
-        dataJSon.put("nodes",NODE);
-        dataJSon.put("links",LINK);
+        dataJSon.put("nodes",nodes);
+        dataJSon.put("links",links);
 
         writeToJson(nodesPath, nodes);
         writeToJson(linksPath, links);
